7e0ddf0f16020fdff78a6d8e7f02f820b260b6c9
hendpraz <mhendryp99@gmail.com>
2020-05-14 17:05:00 +0700

    Add sentry error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,41 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({dsn: "https://dd5c6df52853452eafbc301e81f7310c@o392607.ingest.sentry.io/5240440"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
 export function onError(error) {
-    let message = error.toString();
-  
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
-  
-    alert(message);
+  let errorInfo = {};
+  let message = error.toString();
+
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
+
+  logError(error, errorInfo);
+
+  alert(message);
 }
